a2f0a4c2e8f35b9843fe8e8027b95f8fbf9bb9d8
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-09-12 18:52:04 -0400

    Part 2 MVP Has Been Reached

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from "react";
+import styled from "styled-components"
+
+const HeaderText = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  color: #0F52BA;
+  width: 600px;
+`;
 
 const Header = props => {
   return (
     <div className="header">
-      <h2>NASA Astronomy Picture of the Day.</h2>
+      <HeaderText>NASA Astronomy Picture of the Day.</HeaderText>
     </div>
   );
 };
